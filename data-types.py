9bf0eafa78536fdf92a19b1387bfb1f23c4604e9
KASUDI780 <kasudicharlene@gmail.com>
2025-02-11 19:53:41 +0300

    added files

---
+++ data-types.py
@@ -0,0 +1,19 @@
+my_string = "eMobilis" #string
+my_int = 123 #integer
+my_float = 3.14 #float
+my_bool = True #boolean
+my_bool2 = False #boolean
+
+my_list = ["Kasudi","Hellen","Aisha","Joy","Kesly"] #list
+my_tuple = ("Mangoes","Apples","Oranges","Peaches") #tuple
+my_set = {"Mercedes","Toyota","Nissan","Subaru"} #set
+my_dict = {"name":"Kasudi","age":17,"gender":"female"} #dictionaries
+
+print(type(my_dict))
+print(type(my_string))
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_list))
+print(type(my_tuple))
+print(type(my_set))
