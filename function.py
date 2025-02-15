1b18d119f510fe77b4dd9af52a558c2a3e967577
KASUDI780 <kasudicharlene@gmail.com>
2025-02-15 07:00:00 +0300

    added file

---
+++ function.py
@@ -0,0 +1,15 @@
+def kasudi():
+    print("Hello this is my  function")
+kasudi()
+
+def calculatetwonum():
+    num=int(input("Enter a number: "))
+    num2=int(input("Enter another number: "))
+    print(num+num2)
+calculatetwonum()
+kasudi()
+
+# function with parameters
+def product_number(num3,num4):
+    return num3*num4
+print(product_number(3,4))
---
+++ function2.py
@@ -0,0 +1,17 @@
+def my_function(name,age,gender):
+    print(f"My name is {name} I  am {age} years and my gender is {gender}")
+my_function("Kasudi",17,"female")
+my_function("Joy",18,"Female")
+my_function("Nick",19,"Male")
+
+# create a function called cars with model make year colour as parameters
+# then print a statement "the model the make manufactured in colour "
+# finally call the function 5 times with different arguments
+
+def cars(model,make,year,colour):
+    print(f"The cars model is {model} ,the make is {make} ,manufactured in the year  {year} and it's  {colour} in colour")
+cars("Ford", "BMW", 2023 ,"navy blue")
+cars("BMW", "BMW",  2015,"black")
+cars("honda", "BMW", 2010, "jungle green")
+cars("niche", "BMW", 2024 ,"royal blue")
+cars("Mazdar", "BMW", 2019,"white")
